296dc361799904c173d1c3d0eca6c57fd9e97a8a
Volodymyr Kovalov <kovalyov.volodymyr@gmail.com>
2019-01-15 22:46:23 +0200

    add preliminary app view

--- book/urls.py
+++ book/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from booklib import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'', views.index, name='index'),
 ]
