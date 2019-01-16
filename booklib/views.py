3ebd5b60b43c129990cf6f9c88871d7d72dc324a
Volodymyr Kovalov <kovalyov.volodymyr@gmail.com>
2019-01-16 11:58:36 +0200

    add index.html

--- booklib/views.py
+++ booklib/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
+from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    my_dict = {'insert_me': 'Hello! Im a value from views.py'}
+    return render(request,'booklib/index.html', context=my_dict)
 
 def booklib_index(request):
     return HttpResponse("Booklib index page")
-
-def index(request):
-    return HttpResponse("Hello world")
