b099b817b929b1e7af270ef4f8cfa1219807223d
Volodymyr Kovalov <kovalyov.volodymyr@gmail.com>
2019-01-16 12:59:52 +0200

    add models registration at admin

--- booklib/admin.py
+++ booklib/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from booklib.models import Genre, Author, Book, BookInstance
+
+admin.site.register(Genre)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(BookInstance)
